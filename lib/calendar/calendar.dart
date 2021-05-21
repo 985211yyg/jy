81784640173b2fbb156f1e5e52e0be91249e6ef3
985211yyg <2289201033@qq.com>
2021-05-21 14:05:12 +0800

    first

---
+++ lib/calendar/calendar.dart
@@ -0,0 +1,27 @@
+/// The Syncfusion Flutter Calendar widget has built-in configurable views that
+/// provide basic functionalities for scheduling and representing appointments/events efficiently.
+///
+/// To use, import `package:syncfusion_flutter_calendar/calendar.dart`.
+///
+/// See also:
+/// * [Syncfusion Flutter Calendar product page](https://www.syncfusion.com/flutter-widgets/flutter-calendar)
+/// * [User guide documentation](https://help.syncfusion.com/flutter/calendar/overview)
+/// * [Knowledge base](https://www.syncfusion.com/kb/flutter)
+library calendar;
+
+export 'src/appointment_engine/appointment.dart';
+export 'src/appointment_engine/calendar_datasource.dart';
+export 'src/appointment_engine/recurrence_properties.dart';
+export 'src/common/calendar_controller.dart';
+export 'src/common/enums.dart';
+export 'src/common/event_args.dart';
+export 'src/resource_view/calendar_resource.dart';
+export 'src/settings/header_style.dart';
+export 'src/settings/month_view_settings.dart';
+export 'src/settings/resource_view_settings.dart';
+export 'src/settings/schedule_view_settings.dart';
+export 'src/settings/time_region.dart';
+export 'src/settings/time_slot_view_settings.dart';
+export 'src/settings/view_header_style.dart';
+
+export 'src/sfcalendar.dart';
---
+++ lib/core/tooltip_internal.dart
@@ -0,0 +1,11 @@
+library tooltip_internal;
+
+import 'dart:ui';
+import 'package:flutter/foundation.dart' show kIsWeb;
+import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'core.dart';
+
+part './src/tooltip/tooltip.dart';
